48bf0ec31d212df7d537872f558d64dbf459f386
v12technology <greg.higgins@v12technology.com>
2017-08-26 11:51:45 +0100

    HDR profiler ongoing

--- runtime-plugins/src/test/java/com/fluxtion/runtime/plugin/profiler/HdrHistogramTest.java
+++ runtime-plugins/src/test/java/com/fluxtion/runtime/plugin/profiler/HdrHistogramTest.java
@@ -1,15 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2017 V12 Technology Limited (greg.higgins@v12technology.com)
+ *
+ * This file is part of Fluxtion.
+ *
+ * Fluxtion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.fluxtion.runtime.plugin.profiler;
 
-import java.util.Random;
 import org.HdrHistogram.Histogram;
 import org.HdrHistogram.HistogramLogWriter;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -19,9 +31,10 @@ public class HdrHistogramTest {
 
     
     @Test
+    @Ignore
     public void histoTest(){
         Histogram histogram = new Histogram(5);
-        for (int i = 0; i < 10 * 1000; i++) {
+        for (int i = 1; i <= 10 * 1000; i++) {
             final long value = i;//(long) (1000* Math.random());
 //            System.out.println(value);
             histogram.recordValue(value);
