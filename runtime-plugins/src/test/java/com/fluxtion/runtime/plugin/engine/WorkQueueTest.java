15a25d6fc5699afbb09f5280b93d10f3b8f801ab
greg <greg.higgins@v12technology.com>
2017-09-09 08:25:58 +0100

    starting workqueue implementation

---
+++ runtime-plugins/src/test/java/com/fluxtion/runtime/plugin/engine/WorkQueueTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fluxtion.runtime.plugin.engine;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Greg Higgins (greg.higgins@V12technology.com)
+ */
+public class WorkQueueTest {
+    
+    Logger logger = LoggerFactory.getLogger(WorkQueueTest.class);
+    
+    @Test
+    public void workTest(){
+        
+        ExecutorService consumer = Executors.newSingleThreadExecutor();
+        Future<Future<String>> submit = consumer.submit(() -> this.processData());
+        
+        
+        
+    }
+
+    private Future<String> processData() {
+        return new Future<String>() {
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean isCancelled() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean isDone() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public String get() throws InterruptedException, ExecutionException {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public String get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        }
+    }
+}
