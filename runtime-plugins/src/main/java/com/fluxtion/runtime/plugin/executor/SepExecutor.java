f6a41178c89dde157515b9920b0496602b0936a6
v12technology <greg.higgins@v12technology.com>
2017-09-10 09:10:53 +0100

    FLUX more work on sep executor

--- runtime-plugins/src/main/java/com/fluxtion/runtime/plugin/executor/SepExecutor.java
+++ runtime-plugins/src/main/java/com/fluxtion/runtime/plugin/executor/SepExecutor.java
@@ -20,6 +20,7 @@ package com.fluxtion.runtime.plugin.executor;
 
 import com.fluxtion.runtime.event.Event;
 import com.fluxtion.runtime.lifecycle.EventHandler;
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
@@ -29,75 +30,158 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.LockSupport;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * An executor for the
+ * An executor for the a registered static event processor (SEP). The
+ * SepExecutor provides the following functionality:
+ *
+ * <ul>
+ * <li>Register a SEP for processing/scheduling
+ * <li>Thread safe interaction with the SEP. All SEP processing takes place on a
+ * single thread
+ * <li>Tasks can be submitted for processing {@link #submit(Callable) }
+ * <li>Registered {@link EventSource} are read and Events are processed by the
+ * registered SEP.
+ * <li>Control of scheduling policy:
+ * <ul>
+ * <li>setSleepMicros : sleep between each service loop
+ * <li>useBusySpin : no sleep between each service loop, keep a cpu 100% busy
+ * <li>waitOnTaskQueue : service loop wakes when a task is submitted
+ * </ul>
+ * </ul>
+ *
+ * The default scheduling strategy is setSleepMicros with value of 100_000
+ * micros (100 milliseconds)
  *
  * @author Greg Higgins (greg.higgins@V12technology.com)
  */
 public class SepExecutor {
-    
-    private EventHandler targetSep;
+
+    private final EventHandler targetSep;
     private final String name;
-    private BlockingQueue<FutureTask> queue = new ArrayBlockingQueue<>(10);
-    private AtomicBoolean run = new AtomicBoolean(true);
-    
+    private final BlockingQueue<FutureTask> queue = new ArrayBlockingQueue<>(10);
+    private final AtomicBoolean run = new AtomicBoolean(true);
+    private final AtomicBoolean waitiOnTask = new AtomicBoolean(false);
+
     private final AtomicLong sleepMicros;
     private final Logger loggerRequests;
     private final Logger loggerTasks;
-    private EventSourceDecorator eventSource;
-    
+
+    private final ArrayList<EventSourceDecorator> sourceList;
+    private EventSourceDecorator[] sourceArray;
+
     public SepExecutor(EventHandler targetSep, String name) {
-        loggerRequests = LoggerFactory.getLogger("SepExecutor-requesthandler-" + name);
-        loggerTasks = LoggerFactory.getLogger("SepExecutor-TaskProcessor-" + name);
+        loggerRequests = LoggerFactory.getLogger("sep." + name + ".put");
+        loggerTasks = LoggerFactory.getLogger("sep." + name + ".take");
         this.targetSep = targetSep;
         this.name = name;
         sleepMicros = new AtomicLong(100 * 1000);
+        sourceList = new ArrayList<>();
+        sourceArray = new EventSourceDecorator[0];
         start();
     }
-    
-    public void registerEventSource(EventSource eventSource) {
-        this.eventSource = new EventSourceDecorator(eventSource);
+
+    public void registerEventSource(EventSource eventSource, String name) {
+        submit(() -> {
+            loggerTasks.info("adding EventSource:" + name);
+            sourceList.add(new EventSourceDecorator(eventSource));
+            sourceArray = sourceList.toArray(sourceArray);
+            return null;
+        });
     }
-    
+
     public void shutDown() {
+        loggerRequests.info("shutting down");
         run.lazySet(false);
+        useBusySpin();
+        submit(() -> null);
     }
-    
+
+    /**
+     * Service loop will pause between each iteration. If value is &lt;= 0 this
+     * is equivalent to calling {@link #useBusySpin() }
+     *
+     * @param sleepMicros the pause between loop iterations
+     */
     public void setSleepMicros(long sleepMicros) {
         sleepMicros = sleepMicros < 0 ? 0 : sleepMicros;
+        loggerRequests.info("setting scheduling: setSleepMicros {}", sleepMicros);
         this.sleepMicros.lazySet(sleepMicros);
     }
-    
+
+    /**
+     * Busy spin cpu checking all {@link EventSource}'s and tasks submitted by
+     * {@link #submit(Callable) }
+     */
+    public void useBusySpin() {
+        loggerRequests.info("setting scheduling: useBusySpin");
+        setSleepMicros(0);
+        waitiOnTask.lazySet(false);
+    }
+
+    /**
+     * Progress will only be made when tasks are submitted, using submit
+     */
+    public void waitOnTaskQueue() {
+        loggerRequests.info("setting scheduling: waitOnTaskQueue");
+        waitiOnTask.set(true);
+        setSleepMicros(0);
+    }
+
+    /**
+     * Submit tasks to the registered SEP for processing. Tasks will interact
+     * with the SEP asynchronously on the SEP processor thread. A {@link Future}
+     * is returned encapsulating any return value, accessing via
+     * {@link Future#get}
+     *
+     * @param <T> The return value of the task if any
+     * @param task The task to interract with the SEP
+     * @return The Future value of the task
+     */
     public <T> Future<T> submit(Callable<T> task) {
         loggerRequests.info("submitting task");
         FutureTask<T> ft = new FutureTask(task);
         queue.add(ft);
         return ft;
     }
-    
+
     private void start() {
         loggerTasks.info("starting");
-        Thread t = new Thread("fred") {
+        Thread t = new Thread(name) {
             @Override
             public void run() {
                 while (run.get()) {
-                    try {
-                        //process commands
-                        loggerTasks.info("waiting to take task");
-                        FutureTask future = queue.take();
-                        loggerTasks.info("processing task");
-                        future.run();
-                        //test for read
+                    loggerTasks.trace("starting event loop");
+                    //read EventSource's
+                    for (EventSourceDecorator eventSource : sourceArray) {
                         eventSource.run();
-                        LockSupport.parkNanos(sleepMicros.get() * 1000);
-                    } catch (InterruptedException ex) {
-                        java.util.logging.Logger.getLogger(SepExecutor.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    //process commands
+                    FutureTask future = null;
+                    if (waitiOnTask.get()) {
+                        loggerTasks.trace("waiting task queue take");
+                        try {
+                            future = queue.take();
+                        } catch (InterruptedException ex) {
+                            loggerTasks.info("task queue take interrupted");
+                        }
+                    } else {
+                        loggerTasks.trace("polling task queue");
+                        future = queue.poll();
+                    }
+                    if (future != null) {
+                        loggerTasks.trace("processing task");
+                        future.run();
+                    }
+                    long sleep = sleepMicros.get();
+                    if (sleep > 0) {
+                        loggerTasks.trace("sleeping");
+                        LockSupport.parkNanos(sleep * 1000);
                     }
                 }
+                loggerTasks.info("exiting event loop");
             }
         };
         t.start();
@@ -107,41 +191,45 @@ public class SepExecutor {
      *
      */
     private class EventSourceDecorator implements Runnable {
-        
+
         EventSource eventSource;
-        
+
         public EventSourceDecorator(EventSource eventSource) {
             this.eventSource = eventSource;
         }
-        
+
         @Override
         public void run() {
             try {
-                loggerTasks.info("reading");
+                loggerTasks.trace("reading");
                 Event event = eventSource.read();
                 if (event != null) {
                     targetSep.onEvent(event);
                 } else {
-                    loggerTasks.info("no event to read");
+                    loggerTasks.trace("no event to read");
                 }
             } catch (Exception e) {
                 loggerTasks.error("problem reading event source", e);
             }
         }
     }
+
+    static int count = 0;
     
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        SepExecutor sepExecutor = new SepExecutor(EventHandler.NULL_EVENTHANDLER, "dummyHandler");
-        sepExecutor.setSleepMicros(500 * 1000);
-        
+        SepExecutor sepExecutor = new SepExecutor(EventHandler.NULL_EVENTHANDLER, "sample");
+//        sepExecutor.setSleepMicros(500 * 1000);
         sepExecutor.registerEventSource(() -> {
-            sepExecutor.loggerTasks.info("dummy read");
+            sepExecutor.loggerTasks.info("EventSource dummy read:{}", count++);
             return null;
-        });
-        
-        Thread.sleep(2000);
+        }, "dummyEventSource");
+
+        Thread.sleep(1000);
+        System.out.println("setting waitiOnTask strategy");
+        sepExecutor.waitOnTaskQueue();
+        Thread.sleep(1000);
         Future<String> futureResult = sepExecutor.submit(() -> {
-            sepExecutor.loggerTasks.info("random request");
+            sepExecutor.loggerTasks.info("process my task");
             Thread.sleep(1000);
             return "complete";
         });
@@ -149,7 +237,7 @@ public class SepExecutor {
         sepExecutor.loggerRequests.info("result from task:" + result);
         System.out.println("shutting down");
         sepExecutor.shutDown();
-        
+
     }
-    
+
 }
